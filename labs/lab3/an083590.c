0fea85f652057437eacf374eb4af018ff29bfeff
andrehebra <52555960+andrehebra@users.noreply.github.com>
2023-06-02 20:46:28 +0000

    working on lab 3

---
+++ labs/lab3/an083590.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+typedef struct node {
+  int data;
+  struct node *next;
+} node;
+void markEven(node *head) {
+  node* tempNode = head;
+  
+
+  while (tempNode != NULL) {
+    if (tempNode->data % 2 == 0) {
+      node *add = malloc(sizeof(node));
+    
+      add->data = -1;
+      add->next = tempNode->next;
+      //tempNode->next = add;
+    }
+    
+    tempNode++;
+  }
+  
+}
+void deallocateList(node **head) {
+  node *tmp = *head;
+  while (tmp != NULL) {
+    *head = tmp->next;
+    free(tmp);
+    tmp = *head;
+  }
+  *head = NULL;
+}
+int insertList(node **head, int data) {
+  node *newNode = (node *)malloc(sizeof(node));
+  if (newNode == NULL)
+    return -1;
+  newNode->data = data;
+  newNode->next = NULL;
+  newNode->next = *head;
+  *head = newNode;
+  return 1;
+}
+void printList(node *head) {
+  node *tmp = head;
+  while (tmp != NULL) {
+    printf("%d", tmp->data);
+    if (tmp->next != NULL) {
+      printf(", ");
+    } else {
+      printf(".\n");
+    }
+    tmp = tmp->next;
+  }
+}
+int main(void) {
+  node *head = NULL;
+  insertList(&head, 8);
+  insertList(&head, 3);
+  insertList(&head, 1);
+  insertList(&head, 7);
+  insertList(&head, 6);
+  insertList(&head, 2);
+  printf("Initial List:\n");
+  printList(head);
+  markEven(head);
+  printf("Marked List:\n");
+  printList(head);
+  deallocateList(&head);
+}
